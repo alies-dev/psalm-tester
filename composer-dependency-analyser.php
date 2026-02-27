312cfc04b138d6c8475147e2d078f591d487b658
Alies Lapatsin <web.lapateen@gmail.com>
2026-02-27 19:08:23 +0100

    Use shipmonk/composer-dependency-analyser

---
+++ composer-dependency-analyser.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
+
+// command: composer dependency-analyser
+
+return new Configuration();
+
+// command: composer dependency-analyser
